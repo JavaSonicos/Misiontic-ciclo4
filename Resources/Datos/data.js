d0de4e159a084b7dd20e86ff7256b4077d33f4a6
fabiandres14 <fabianbm147@gmail.com>
2022-10-22 22:02:58 -0500

    re estructurando y agregando,luego se modifica-Fabian_Borrero

---
+++ Resources/Datos/data.js
@@ -0,0 +1,12 @@
+export const proyecto=[
+    {
+       Admin_1:  'Vista administrador - ventas',
+
+       Admin_2:  'Vista administrador - modificar',
+
+       Cliente_1: 'Vista cliente - carrito',
+
+       Cliente_2: 'Vista cliente - lista de productos'
+    
+    }
+]
---
+++ Resources/index.js
@@ -0,0 +1,2 @@
+console.log("Prueba la consola del navegador")
+alert("Re estructuracion del proyecto,luego se modificara")
